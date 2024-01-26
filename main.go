75f75cd3261ba7e96e9983225677fccefd70af9f
Goutam Verma <goutam.verma@bobble.ai>
2024-01-26 17:02:56 +0530

    inital-commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(200, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
